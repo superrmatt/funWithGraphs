261fbf622ddff3193fa1c25f257656dbec1eabd6
Matt Dambra <mpdambra@gmail.com>
2020-01-17 20:09:47 -0500

    a few comments, while to go with the commenting

--- Node.java
+++ Node.java
@@ -14,29 +14,47 @@ public class Node {
      *            / \
      *           2   3
      * child looks like: [2,3]
-     * Further, with the following node 1:
-     *             4
-     *              \
-     *               1
-     *              / \
-     *             2   3
-     * child looks like: [2,3,4]
      */
-	public Node[] child;
+    public Node[] child;
+    
+    /**
+     * How many children a Node has
+     */
     public int childCount;
+
+    /**
+     * vertex of type string
+     */
     private String vertex;
+
+    /**
+     * state of type State
+     */
     public State state;
 
+    /**
+     * Ah, lovely constructors!
+     * @arg string, the vertex of this
+     */
     public Node(String vertex){
         this.vertex = vertex;
     }
 
+    /**
+     * Another constructor! Woohoo!
+     * @arg: string, the vertex
+     * @arg length of child as type int. This is where things get tricky, eh?
+     */
     public Node(String vertex, int childlen){
         this.vertex = vertex;
         childCount = 0;
         child = new Node[childlen];
     }
 
+    /**
+     * Add a child node 
+     * @param type Node, the node to add.
+     */
     public void addChildNode(Node adj){
         adj.state = State.Unvisited;
         if(childCount < 30)

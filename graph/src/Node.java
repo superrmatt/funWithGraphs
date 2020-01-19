e507c2ac606a8695be9c0c5fc2f2cb156d583505
Matt Dambra <mpdambra@gmail.com>
2020-01-18 20:15:53 -0500

    updating node.java with comments and some other like

--- graph/src/Node.java
+++ graph/src/Node.java
@@ -12,13 +12,31 @@ public class Node<T> {
      * Value of type T. Often referred to as vertex in graph theory.
      */
     private T value;
+
+    /**
+     * Set<Node<T>> of this Node's neighbors.
+     */
     private Set<Node<T>> neighbors;
  
+    /**
+     * Constructor
+     * @param value of type T
+     */
     public Node(T value) {
         this.value = value;
         this.neighbors = new HashSet<>();
     }
  
+    /**
+     * creates a connection. in Graph theory a connection is referred to as an edge.
+     * It would be the line drawn from one Node (vertex) to another. Like so:
+     * Let Graph G = (V, E) where V is set of vertices (Nodes) and E is set of edges.
+     * Connect builds E, the edges. lets's say graph G has vertices (A, B, C) and Edges (1, 2, 3).
+     * We can illustrate this as so:
+     *               A
+     *              / \
+     *             B - C
+     */
     public void connect(Node<T> node) {
         if (this == node) throw new IllegalArgumentException("Can't connect node to itself");
         this.neighbors.add(node);
